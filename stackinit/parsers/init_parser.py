e6131d085b2f88b75ef52243d55d4783970909de
Snekchyk <snekchyk@gmail.com>
2025-10-25 22:56:01 +0300

    Modified init_parser.py, add check function

--- stackinit/parsers/init_parser.py
+++ stackinit/parsers/init_parser.py
@@ -1,4 +1,6 @@
 from rich import print
+import os
+import json
 
 class InitParser():
     def __init__(self, project_name, is_new, template, pkgmgr):
@@ -7,10 +9,37 @@ class InitParser():
         self.template = template
         self.pkgmgr = pkgmgr
 
+        self.data_path = os.path.join(os.path.dirname(__file__), "..", "data", "templates_list.json")
+
+
     def run(self):
         print(f"\n{"" if not self.is_new else "New "}{"Project" if not self.is_new else "project"} [bold]{self.project_name}[/bold] will create with\ntemplate: [bold]{self.template}[/bold],\npackage manager: [bold]{self.pkgmgr}[/bold]\n")
         self.check()
 
 
     def check(self):
-        print("debugger")
+        with open(self.data_path, 'r', encoding='utf-8') as file:
+            content = json.load(file)
+        
+        templates = content.get("templates")
+
+
+        if self.template not in templates:
+            print("There isn`t the same template")
+        else:
+            print("Succesfuly set template")
+            package_manager = templates.get(self.template)
+            if self.pkgmgr == "unknown":
+                print("Package manager is default")
+                self.run_exec()
+            else:
+
+                if self.pkgmgr not in package_manager["available_pkgmgr"]:
+                    print("There isn`t the same package manager in base")
+                else:
+                    print("Succesfuly set package manager")
+                    self.run_exec()
+
+
+    def run_exec(self):
+        print("run exec has started")
