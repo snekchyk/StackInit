4014f53421615b6ad64dfcc805aac7489b7f5d03
Snekchyk <snekchyk@gmail.com>
2025-10-25 13:33:52 +0300

    Created temlates_list.json and add basic code of init_parser.py

--- cli.py
+++ cli.py
@@ -1,28 +1,37 @@
 import typer
 from rich import print
+from parsers.init_parser import InitParser
 
 app = typer.Typer()
 
-@app.command()
-def init(project_name: str,
-        is_new: bool = typer.Option(False, "--new", "-n"),
-        template: str = typer.Option("unknown", "--template", "-t"), 
-        pkgmgr: str = typer.Option("unknown", "--pkgmgr", "-p")):
-    
+class cli():
+    @app.command()
+    def init(project_name: str,
+            is_new: bool = typer.Option(False, "--new", "-n"),
+            template: str = typer.Option("unknown", "--template", "-t"), 
+            pkgmgr: str = typer.Option("unknown", "--pkgmgr", "-p")):
+                
+        # cli.py init -> init_parser.py 
+        #project_name
+        #is_new
+        #template
+        #pkgmgr
 
-    print(f"{"" if not is_new else "new"} project {project_name} has created with template: {template}, package manager: {pkgmgr}")
+        parser = InitParser(project_name=project_name, is_new=is_new, template=template, pkgmgr=pkgmgr)
+        parser.run()
 
-@app.command()
-def add():
-    pass
 
-@app.command()
-def list():
-    pass
+    @app.command()
+    def add():
+        pass
 
-@app.command()
-def help():
-    pass
+    @app.command()
+    def list():
+        pass
+
+    @app.command()
+    def help():
+        pass
     
 if __name__ == "__main__":
     app()
---
+++ parsers/init_parser.py
@@ -0,0 +1,16 @@
+from rich import print
+
+class InitParser():
+    def __init__(self, project_name, is_new, template, pkgmgr):
+        self.project_name = project_name
+        self.is_new = is_new
+        self.template = template
+        self.pkgmgr = pkgmgr
+
+    def run(self):
+        print(f"\n{"" if not self.is_new else "New "}{"Project" if not self.is_new else "project"} [bold]{self.project_name}[/bold] will create with\ntemplate: [bold]{self.template}[/bold],\npackage manager: [bold]{self.pkgmgr}[/bold]\n")
+        self.check()
+
+
+    def check(self):
+        pass
